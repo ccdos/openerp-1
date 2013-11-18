8128ea1812e6decc42cdc895d7d0522cc3672e61
Jose miguel Esteve esteve <jmesteveme.com>
2013-11-18 01:00:35 +0100

    icono logotipo funciona en todos los headers

--- openerp/addons_extra/report_webkit_7/report/purchase_webkit.py
+++
@@ -1,8 +0,0 @@
-from openerp.report import report_sxw
-
-class purchase_webkit_report(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(purchase_webkit_report, self).__init__(cr, uid, name, context=context)
-
-report_sxw.report_sxw('report.purchase.rep.webkit','purchase.order','addons/report_webkit_7/report/purchase_rep_webkit.mako',parser=purchase_webkit_report) 
-
