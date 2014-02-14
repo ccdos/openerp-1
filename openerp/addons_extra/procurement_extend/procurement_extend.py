46d0e3feeee5b616a449b4d48f8cc0335704d795
Jose miguel Esteve esteve <jmesteveme.com>
2014-02-14 15:49:59 +0100

    procurement

--- openerp/addons_extra/procurement_extend/procurement_extend.py
+++ openerp/addons_extra/procurement_extend/procurement_extend.py
@@ -1,4 +1,5 @@
 from openerp.osv import fields, osv
+from openerp import netsvc
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from openerp.tools import  DEFAULT_SERVER_DATETIME_FORMAT
@@ -14,6 +15,9 @@ class procurement_order(osv.osv):
     _inherit = "procurement.order"
     _columns ={
                'date_order': fields.datetime('Order date', required=True, select=True),
+               }
+    _defaults ={
+               'location_id':7,
                }
     
     def _get_purchase_order_date(self, cr, uid, procurement, company, schedule_date, context=None):
@@ -68,3 +72,58 @@ class procurement_order(osv.osv):
                 self.write(cr, uid, [reg.id], {'state': 'draft'})
                 return True
         return False
+procurement_order()
+
+class make_procurement(osv.osv_memory):
+    _inherit = 'make.procurement'
+    
+    def make_procurement(self, cr, uid, ids, context=None):
+        """ Creates procurement order for selected product.
+        @param self: The object pointer.
+        @param cr: A database cursor
+        @param uid: ID of the user currently logged in
+        @param ids: List of IDs selected
+        @param context: A standard dictionary
+        @return: A dictionary which loads Procurement form view.
+        """
+        user = self.pool.get('res.users').browse(cr, uid, uid, context=context).login
+        wh_obj = self.pool.get('stock.warehouse')
+        procurement_obj = self.pool.get('procurement.order')
+        wf_service = netsvc.LocalService("workflow")
+        data_obj = self.pool.get('ir.model.data')
+        product_obj = self.pool.get('product.template')
+        
+        for proc in self.browse(cr, uid, ids, context=context):
+            wh = wh_obj.browse(cr, uid, proc.warehouse_id.id, context=context)
+            product_id = proc.uom_id.id
+            product_obj = product_obj.browse(cr, uid, product_id, context=context)
+            procure_id = procurement_obj.create(cr, uid, {
+                'name':'INT: '+str(user),
+                'date_planned': proc.date_planned,
+                'product_id': proc.product_id.id,
+                'product_qty': proc.qty,
+                'product_uom': proc.uom_id.id,
+                #'location_id': wh.lot_stock_id.id,
+                'location_id': product_obj.property_stock_production.id,
+                'procure_method':'make_to_order',
+            })
+    
+            wf_service.trg_validate(uid, 'procurement.order', procure_id, 'button_confirm', cr)
+    
+    
+        id2 = data_obj._get_id(cr, uid, 'procurement', 'procurement_tree_view')
+        id3 = data_obj._get_id(cr, uid, 'procurement', 'procurement_form_view')
+    
+        if id2:
+            id2 = data_obj.browse(cr, uid, id2, context=context).res_id
+        if id3:
+            id3 = data_obj.browse(cr, uid, id3, context=context).res_id
+    
+        return {
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'procurement.order',
+            'res_id' : procure_id,
+            'views': [(id3,'form'),(id2,'tree')],
+            'type': 'ir.actions.act_window',
+         }
